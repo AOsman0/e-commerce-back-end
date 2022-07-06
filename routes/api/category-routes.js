c2deac05bbd79749224884c93b176871ecf30fd8
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-07-06 17:55:57 +0100

    category model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,20 +5,27 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  // use find all method 
+  const categories = await Book.findAll();
+
   // be sure to include its associated Products
+  return res.json(categories);
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  // use find one method 
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // use create method 
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  // use built in update method 
 });
 
 router.delete('/:id', (req, res) => {
